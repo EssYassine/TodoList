38bdae393d53d462dd4070088d77b4ff44e3aa5f
Yassine Essaouri <essaouri.yassine@gmail.com>
2024-11-22 22:09:37 +0100

    Add project files.

---
+++ TodoList/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+Console.ReadKey();
